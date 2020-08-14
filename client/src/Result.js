ce43e7ba4b7ac19f5aec8e9681947c107b63c79a
Cindy Zhang <cindyzhang977@gmail.com>
2020-08-13 20:04:24 -0700

    result style and render

--- client/src/Result.js
+++ client/src/Result.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import ReactHtmlParser from 'react-html-parser'
 import styles from './styles/Result.module.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import SearchResults from './SearchResults'
 
-function Result() {
+function Result({ result }) {
+    console.log(result)
     return (
         <div className={styles.container}>
-            Result here
+            <Container>
+                <Row>
+                    <Col xs={6} sm={8} lg={9} className={styles.question}>
+                        {result.question}
+                    </Col>
+                    <Col xs={6} sm={4} lg={3} className={styles.toggleArrowsContainer}>
+                        <Button className={styles.toggleArrow}>
+                            prev
+                            <FontAwesomeIcon icon={faAngleUp} />
+                        </Button>
+                        <Button className={styles.toggleArrow}>
+                            next
+                            <FontAwesomeIcon icon={faAngleDown} />
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+            <div>
+                <div className={styles.label}>Post</div>
+                {ReactHtmlParser(result.post)}
+            </div>
+            <div>
+                <div className={styles.label}>Answer</div>
+                {ReactHtmlParser(result.answer)}
+            </div>
+            <div className={styles.urlBtnContainer}>
+                <Button href={result.url} target="_blank" rel="noopener noreferrer" className={styles.urlBtn}>
+                    View full post
+                </Button>
+            </div>
         </div>
     )
 }
--- client/src/SearchResults.js
+++ client/src/SearchResults.js
@@ -4,6 +4,8 @@ import queryString from 'query-string'
 import styles from './styles/SearchResults.module.css'
 import Spinner from 'react-bootstrap/Spinner'
 
+import data from './data.js';
+
 import Result from './Result.js'
 
 function SearchResults() {
@@ -12,21 +14,21 @@ function SearchResults() {
     const [results, setResults] = useState(null)
     const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        if (!query) {
-            console.log("no query")
-            return
-        }
-        fetch(`/scrape-query/${query}`)
-            .then(res => res.json())
-            .then(json => {
-                console.log(json)
-                setResults(json.results)
-            })
-            .then(() => {
-                setLoading(false)
-            })
-    }, [query])
+    // useEffect(() => {
+    //     if (!query) {
+    //         console.log("no query")
+    //         return
+    //     }
+    //     fetch(`/scrape-query/${query}`)
+    //         .then(res => res.json())
+    //         .then(json => {
+    //             console.log(json)
+    //             setResults(json.results)
+    //         })
+    //         .then(() => {
+    //             setLoading(false)
+    //         })
+    // }, [query])
 
     return (
         <div className={styles.container}>
@@ -34,7 +36,7 @@ function SearchResults() {
                 Search results for: <span className={styles.question}>{query}</span>
             </div>
             {
-                loading ? 
+                !loading &&
                     <div className={styles.loadingContainer}>
                         <Spinner animation="border" role="status" className={styles.spinner}>
                             <span className="sr-only">Loading...</span>
@@ -43,9 +45,12 @@ function SearchResults() {
                             Scraping... Please wait. 
                         </div>
                     </div>
-                    :
-                    <div>Done</div>
             }
+            <div className={styles.resultsContainer}>
+                {data.map((result, index) =>
+                    <Result result={result} key={index} />
+                )}
+            </div>
         </div>
     )
 }
